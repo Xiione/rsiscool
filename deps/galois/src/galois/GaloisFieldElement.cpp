4ba9423878ede33421aa2cd9cf245315dbda527b
wang5660 <hamiltonlwang12@gmail.com>
2024-06-17 11:48:44 -0700

    Format galois sources

--- deps/galois/src/galois/GaloisFieldElement.cpp
+++ deps/galois/src/galois/GaloisFieldElement.cpp
@@ -1,79 +1,64 @@
 #include "GaloisFieldElement.h"
 
-namespace galois
-{
+namespace galois {
 
-   GaloisFieldElement::GaloisFieldElement(GFSymbol v)
-   {
-      poly_value = v & GF.size();
-   }
-
-   GaloisFieldElement::GaloisFieldElement(const GaloisFieldElement& gfe)
-   {
-      poly_value  = gfe.poly_value;
-   }
-
-
-   std::ostream& operator << (std::ostream& os, const GaloisFieldElement& gfe)
-   {
-      os << gfe.poly_value;
-      return os;
-   }
-
-
-   GaloisFieldElement operator+(const GaloisFieldElement& a, const GaloisFieldElement& b)
-   {
-      GaloisFieldElement result  = a;
-      result += b;
-      return result;
-   }
-
-
-   GaloisFieldElement operator-(const GaloisFieldElement& a, const GaloisFieldElement& b)
-   {
-      GaloisFieldElement result  = a;
-      result -= b;
-      return result;
-   }
-
-
-   GaloisFieldElement operator*(const GaloisFieldElement& a, const GaloisFieldElement& b)
-   {
-      GaloisFieldElement result  = a;
-      result *= b;
-      return result;
-   }
-
-
-   GaloisFieldElement operator*(const GaloisFieldElement& a, const GFSymbol& b)
-   {
-      GaloisFieldElement result  = a;
-      result *= b;
-      return result;
-   }
-
-
-   GaloisFieldElement operator*(const GFSymbol& a, const GaloisFieldElement& b)
-   {
-      GaloisFieldElement result  = b;
-      result *= a;
-      return result;
-   }
-
-
-   GaloisFieldElement operator/(const GaloisFieldElement& a, const GaloisFieldElement& b)
-   {
-      GaloisFieldElement result  = a;
-      result /= b;
-      return result;
-   }
-
-
-   GaloisFieldElement operator^(const GaloisFieldElement& a, const int& b)
-   {
-      GaloisFieldElement result  = a;
-      result ^= b;
-      return result;
-   }
-
+GaloisFieldElement::GaloisFieldElement(GFSymbol v) {
+  poly_value = v & GF.size();
 }
+
+GaloisFieldElement::GaloisFieldElement(const GaloisFieldElement &gfe) {
+  poly_value = gfe.poly_value;
+}
+
+std::ostream &operator<<(std::ostream &os, const GaloisFieldElement &gfe) {
+  os << gfe.poly_value;
+  return os;
+}
+
+GaloisFieldElement operator+(const GaloisFieldElement &a,
+                             const GaloisFieldElement &b) {
+  GaloisFieldElement result = a;
+  result += b;
+  return result;
+}
+
+GaloisFieldElement operator-(const GaloisFieldElement &a,
+                             const GaloisFieldElement &b) {
+  GaloisFieldElement result = a;
+  result -= b;
+  return result;
+}
+
+GaloisFieldElement operator*(const GaloisFieldElement &a,
+                             const GaloisFieldElement &b) {
+  GaloisFieldElement result = a;
+  result *= b;
+  return result;
+}
+
+GaloisFieldElement operator*(const GaloisFieldElement &a, const GFSymbol &b) {
+  GaloisFieldElement result = a;
+  result *= b;
+  return result;
+}
+
+GaloisFieldElement operator*(const GFSymbol &a, const GaloisFieldElement &b) {
+  GaloisFieldElement result = b;
+  result *= a;
+  return result;
+}
+
+GaloisFieldElement operator/(const GaloisFieldElement &a,
+                             const GaloisFieldElement &b) {
+  GaloisFieldElement result = a;
+  result /= b;
+  return result;
+}
+
+GaloisFieldElement operator^(const GaloisFieldElement &a, const int &b) {
+  GaloisFieldElement result = a;
+  result ^= b;
+  return result;
+}
+
+} // namespace galois
--- deps/galois/src/galois/GaloisFieldElement.h
+++ deps/galois/src/galois/GaloisFieldElement.h
@@ -1,204 +1,147 @@
 /*
-  *********************************************************************
-  *                                                                   *
-  *        Galois Field Arithmetic Library (version 0.0.1)            *
-  *                                                                   *
-  * Class: Galois Field Element                                       *
-  * Version: 0.0.1                                                    *
-  * Author: Arash Partow - 2000                                       *
-  * URL: http://www.partow.net/projects/galois/index.html             *
-  *                                                                   *
-  * Copyright Notice:                                                 *
-  * Free use of this library is permitted under the guidelines and    *
-  * in accordance with the most current version of the Common Public  *
-  * License.                                                          *
-  * http://www.opensource.org/licenses/cpl.php                        *
-  *                                                                   *
-  *********************************************************************
-*/
-
+ *********************************************************************
+ *                                                                   *
+ *        Galois Field Arithmetic Library (version 0.0.1)            *
+ *                                                                   *
+ * Class: Galois Field Element                                       *
+ * Version: 0.0.1                                                    *
+ * Author: Arash Partow - 2000                                       *
+ * URL: http://www.partow.net/projects/galois/index.html             *
+ *                                                                   *
+ * Copyright Notice:                                                 *
+ * Free use of this library is permitted under the guidelines and    *
+ * in accordance with the most current version of the Common Public  *
+ * License.                                                          *
+ * http://www.opensource.org/licenses/cpl.php                        *
+ *                                                                   *
+ *********************************************************************
+ */
 
 #ifndef INCLUDE_GALOISFIELDELEMENT_H
 #define INCLUDE_GALOISFIELDELEMENT_H
 
-#include <iostream>
-#include <vector>
 #include "GaloisField.h"
 #include "GlobalGaloisField.h"
+#include <iostream>
+#include <vector>
 
-namespace galois
-{
+namespace galois {
 
-   class GaloisFieldElement
-   {
+class GaloisFieldElement {
 
-      public:
+public:
+  GaloisFieldElement(GFSymbol v = -1);
+  GaloisFieldElement(const GaloisFieldElement &gfe);
+  ~GaloisFieldElement() {}
 
-       GaloisFieldElement(GFSymbol v = -1);
-       GaloisFieldElement(const GaloisFieldElement& gfe);
-      ~GaloisFieldElement(){}
+  inline GaloisFieldElement &operator=(const GaloisFieldElement &gfe) {
+    if (this == &gfe)
+      return *this;
 
-       inline GaloisFieldElement& operator=(const GaloisFieldElement& gfe)
-       {
-          if (this == &gfe)
-            return *this;
+    poly_value = gfe.poly_value;
 
-          poly_value  = gfe.poly_value;
+    return *this;
+  }
 
-          return *this;
-       }
+  inline GaloisFieldElement &operator=(const GFSymbol &v) {
+    poly_value = v & GF.size();
+    return *this;
+  }
 
+  inline GaloisFieldElement &operator+=(const GaloisFieldElement &gfe) {
+    poly_value ^= gfe.poly_value;
+    return *this;
+  }
 
-       inline GaloisFieldElement& operator=(const GFSymbol& v)
-       {
-          poly_value  = v & GF.size();
-          return *this;
-       }
+  inline GaloisFieldElement &operator+=(const GFSymbol &v) {
+    poly_value ^= v;
+    return *this;
+  }
 
+  inline GaloisFieldElement &operator-=(const GaloisFieldElement &gfe) {
+    *this += gfe;
+    return *this;
+  }
 
-       inline GaloisFieldElement& operator+=(const GaloisFieldElement& gfe)
-       {
-          poly_value ^= gfe.poly_value;
-          return *this;
-       }
+  inline GaloisFieldElement &operator-=(const GFSymbol &v) {
+    *this += v;
+    return *this;
+  }
 
+  inline GaloisFieldElement &operator*=(const GaloisFieldElement &gfe) {
+    poly_value = GF.mul(poly_value, gfe.poly_value);
+    return *this;
+  }
 
-       inline GaloisFieldElement& operator+=(const GFSymbol& v)
-       {
-          poly_value ^= v;
-          return *this;
-       }
+  inline GaloisFieldElement &operator*=(const GFSymbol &v) {
+    poly_value = GF.mul(poly_value, v);
+    return *this;
+  }
 
+  inline GaloisFieldElement &operator/=(const GaloisFieldElement &gfe) {
+    poly_value = GF.div(poly_value, gfe.poly_value);
+    return *this;
+  }
 
-       inline GaloisFieldElement& operator-=(const GaloisFieldElement& gfe)
-       {
-           *this += gfe;
-           return *this;
-       }
+  inline GaloisFieldElement &operator/=(const GFSymbol &v) {
+    poly_value = GF.div(poly_value, v);
+    return *this;
+  }
 
+  inline GaloisFieldElement &operator^=(const int &n) {
+    poly_value = GF.exp(poly_value, n);
+    return *this;
+  }
 
-       inline GaloisFieldElement& operator-=(const GFSymbol& v)
-       {
-          *this += v;
-          return *this;
-       }
+  inline bool operator==(const GaloisFieldElement &gfe) const {
+    return (poly_value == gfe.poly_value);
+  }
 
+  inline bool operator==(const GFSymbol &v) const { return (poly_value == v); }
 
-       inline GaloisFieldElement& operator*=(const GaloisFieldElement& gfe)
-       {
-          poly_value = GF.mul(poly_value,gfe.poly_value);
-          return *this;
-       }
+  inline bool operator!=(const GaloisFieldElement &gfe) const {
+    return (poly_value != gfe.poly_value);
+  }
 
+  inline bool operator!=(const GFSymbol &v) const { return (poly_value != v); }
 
-       inline GaloisFieldElement& operator*=(const GFSymbol& v)
-       {
-          poly_value = GF.mul(poly_value,v);
-          return *this;
-       }
+  inline bool operator<(const GaloisFieldElement &gfe) {
+    return (poly_value < gfe.poly_value);
+  }
 
+  inline bool operator<(const GFSymbol &v) { return (poly_value < v); }
 
-       inline GaloisFieldElement& operator/=(const GaloisFieldElement& gfe)
-       {
-          poly_value = GF.div(poly_value,gfe.poly_value);
-          return *this;
-       }
+  inline bool operator>(const GaloisFieldElement &gfe) {
+    return (poly_value > gfe.poly_value);
+  }
 
+  inline bool operator>(const GFSymbol &v) { return (poly_value > v); }
 
-       inline GaloisFieldElement& operator/=(const GFSymbol& v)
-       {
-          poly_value = GF.div(poly_value,v);
-          return *this;
-       }
+  inline GFSymbol index() const { return GF.index(poly_value); }
 
+  inline GFSymbol poly() const { return poly_value; }
 
-       inline GaloisFieldElement& operator^=(const int& n)
-       {
-          poly_value = GF.exp(poly_value,n);
-          return *this;
-       }
+  inline GFSymbol inverse() const { return GF.inverse(poly_value); }
 
+  friend std::ostream &operator<<(std::ostream &os,
+                                  const GaloisFieldElement &gfe);
 
-       inline bool operator==(const GaloisFieldElement& gfe) const
-       {
-          return (poly_value == gfe.poly_value);
-       }
+private:
+  GFSymbol poly_value;
+};
 
+GaloisFieldElement operator+(const GaloisFieldElement &a,
+                             const GaloisFieldElement &b);
+GaloisFieldElement operator-(const GaloisFieldElement &a,
+                             const GaloisFieldElement &b);
+GaloisFieldElement operator*(const GaloisFieldElement &a,
+                             const GaloisFieldElement &b);
+GaloisFieldElement operator*(const GaloisFieldElement &a, const GFSymbol &b);
+GaloisFieldElement operator*(const GFSymbol &a, const GaloisFieldElement &b);
+GaloisFieldElement operator/(const GaloisFieldElement &a,
+                             const GaloisFieldElement &b);
+GaloisFieldElement operator^(const GaloisFieldElement &a, const int &b);
 
-       inline bool operator==(const GFSymbol& v) const
-       {
-          return (poly_value == v);
-       }
-
-
-       inline bool operator!=(const GaloisFieldElement& gfe) const
-       {
-          return (poly_value != gfe.poly_value);
-       }
-
-
-       inline bool operator!=(const GFSymbol& v) const
-       {
-          return (poly_value != v);
-       }
-
-
-       inline bool operator<(const GaloisFieldElement& gfe)
-       {
-          return (poly_value < gfe.poly_value);
-       }
-
-
-       inline bool operator<(const GFSymbol& v)
-       {
-          return (poly_value < v);
-       }
-
-       inline bool operator>(const GaloisFieldElement& gfe)
-       {
-          return (poly_value > gfe.poly_value);
-       }
-
-
-       inline bool operator>(const GFSymbol& v)
-       {
-          return (poly_value > v);
-       }
-
-       inline GFSymbol index() const
-       {
-          return GF.index(poly_value);
-       }
-
-
-       inline GFSymbol poly() const
-       {
-          return poly_value;
-       }
-
-
-       inline GFSymbol inverse() const
-       {
-          return GF.inverse(poly_value);
-       }
-
-
-       friend std::ostream& operator << (std::ostream& os, const GaloisFieldElement& gfe);
-
-      private:
-
-       GFSymbol     poly_value;
-
-   };
-
-   GaloisFieldElement operator + (const GaloisFieldElement& a, const GaloisFieldElement& b);
-   GaloisFieldElement operator - (const GaloisFieldElement& a, const GaloisFieldElement& b);
-   GaloisFieldElement operator * (const GaloisFieldElement& a, const GaloisFieldElement& b);
-   GaloisFieldElement operator * (const GaloisFieldElement& a, const GFSymbol& b);
-   GaloisFieldElement operator * (const GFSymbol& a, const GaloisFieldElement& b);
-   GaloisFieldElement operator / (const GaloisFieldElement& a, const GaloisFieldElement& b);
-   GaloisFieldElement operator ^ (const GaloisFieldElement& a, const int& b);
-
-}
+} // namespace galois
 
 #endif
