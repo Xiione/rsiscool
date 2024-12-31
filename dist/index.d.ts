a5c25e9ba3df31ddea9092eaf886b432f66ce4f8
wang5660 <hamiltonlwang12@gmail.com>
2024-12-30 17:44:06 -0800

    Export MainModule type

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,6 @@
-import createModule from "./rsiscool";
+import createModule, { type MainModule } from "./rsiscool";
 import createWorkersModule from "./rsiscool_workers";
+
+export type MainModule = MainModule;
 
 export { createModule, createWorkersModule };
